d737e9af3ada0e7c930aad3c2ae8422b53b3526b
mishakostenko <mishakostenko70@gmail.com>
2024-09-20 14:46:42 +0200

    added models Categories and Events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    reviews_at = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Events(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    reviews_at = models.IntegerField(default=0)
+    price = models.FloatField()
+    date = models.DateField()
+    place = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
