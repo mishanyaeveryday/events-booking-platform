1e4ba78a6bfe62deda85796d9487ea8c8ecacfe0
mishakostenko <mishakostenko70@gmail.com>
2024-09-26 16:52:38 +0200

    Integrated Paypal system for Donate

--- payment/urls.py
+++ payment/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.donate, name='donate'),
+    path('donate-failed', views.DonateFailed, name='donate-failed'),
+    path('donate-success', views.DonateSuccess, name='donate-success'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -9,36 +9,28 @@ from django.urls import reverse
 
 def donate(request):
     categories = Categories.objects.all()
-    return render(request, 'events/donate.html', {'categories': categories})
-
-
-def single_event(request, event_id):
-    event = get_object_or_404(Events, event_id=event_id)
-    categories = Categories.objects.all()
     host = request.get_host()
 
     paypal_checkout = {
         'business': settings.PAYPAL_RECEIVER_EMAIL,
-        'amount': event.price,
-        'item_name': event.title,
+        'amount': 10,
+        'item_name': 'DONATE',
         'invoice': uuid.uuid4(),
         'currency_code': 'USD',
         'notify_url': f"http://{host}{reverse('paypal-ipn')}",
-        'return_url': f"http://{host}{reverse('payment-success', kwargs={'event_id': event_id})}",
-        'cancel_url': f"http://{host}{reverse('payment-failed', kwargs={'event_id': event_id})}",
+        'return_url': f"http://{host}{reverse('donate-success')}",
+        'cancel_url': f"http://{host}{reverse('donate-failed')}",
     }
 
     paypal_payment = PayPalPaymentsForm(initial=paypal_checkout)
-    return render(request, 'events/single_event.html', {'event': event, 'categories': categories, 'paypal': paypal_payment})
+    return render(request, 'events/donate.html', {'categories': categories, 'paypal': paypal_payment})
 
 
-def PaymentSuccess(request, event_id):
-    event = Events.objects.get(event_id=event_id)
+def DonateSuccess(request):
     categories = Categories.objects.all()
-    return render(request, 'payment-success.html', {'event': event, 'categories': categories})
+    return render(request, 'events/donate-success.html', {'categories': categories})
 
 
-def PaymentFailed(request, event_id):
-    event = Events.objects.get(event_id=event_id)
+def DonateFailed(request):
     categories = Categories.objects.all()
-    return render(request, 'payment-failed.html', {'event': event, 'categories': categories})
+    return render(request, 'events/donate-failed.html', {'categories': categories})
